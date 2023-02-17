030d2fb619f03b2f7f71c1c597ff96bdacd5bec7
Fadila-Ali <fadilaali@pursuit.org>
2023-02-17 08:30:28 -0500

    react helmet and favicon added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useState } from "react";
+import { Helmet } from "react-helmet";
 import { FaMoon } from "react-icons/fa";
 import { BsSunFill } from "react-icons/bs";
 import NavBar from "./Components/NavBar";
@@ -23,6 +24,12 @@ function App() {
         darkToggle && "dark"
       }`}
     >
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Mariolle</title>
+        <link rel="canonical" href="http://mysite.com/example"></link>
+        <meta name="description" content="An e-commerce app"></meta>
+      </Helmet>
       <Router>
         <NavBar />
         <main className="text-gray-700 bg-purple-50 dark:bg-[#4C516D] dark:text-gray-100 p-2 h-full">
@@ -43,7 +50,7 @@ function App() {
             <Route path="*" elememt={<Error />} />
           </Routes>
         </main>
-        <ScrollButton/>
+        <ScrollButton />
         <Footer />
       </Router>
     </div>
